9983391e8bb0b4ecf2a58f2c4379b595649635e7
박재완 <wjp1230@naver.com>
2025-03-07 14:55:50 +0900

    Solve: BOJ 14725 개미굴

---
+++ BOJ/트라이/BOJ14725_개미굴.java
@@ -0,0 +1,58 @@
+/*
+ * 개미가 개미굴을 타고 내려가면서 있는 먹이를 파악한다.
+ * 입력 순서대로 Trie 생성 후, dfs 로 출력
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.TreeMap;
+
+public class BOJ14725_개미굴 {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Trie trie = new Trie();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            int t = Integer.parseInt(input[0]);
+            for (int j = 0; j < t; j++)
+                trie.insert(Arrays.copyOfRange(input, 1, t + 1));
+        }
+
+        // dfs 로 순회하며 출력
+        printDfs(trie.root, 0);
+        System.out.println(sb.toString());
+    }
+
+    private static void printDfs(Node cur, int depth) {
+        for (String str : cur.child.keySet()) {
+            for (int i = 0; i < depth; i++)
+                sb.append("--");
+            sb.append(str).append('\n');
+
+            // 다음 노드 dfs 로 탐색
+            printDfs(cur.child.get(str), depth + 1);
+        }
+    }
+
+    static class Node {
+        TreeMap<String, Node> child = new TreeMap<>();
+    }
+
+    static class Trie {
+        Node root = new Node();
+
+        void insert(String[] str) {
+            Node node = root;
+
+            for (int i = 0; i < str.length; i++) {
+                node.child.putIfAbsent(str[i], new Node());
+                node = node.child.get(str[i]);
+            }
+        }
+    }
+}
