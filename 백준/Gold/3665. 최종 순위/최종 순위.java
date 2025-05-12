3ab2fc6c107f07c9673599d64a271bfac7d6e5e7
박재완 <58254234+jaewan1230@users.noreply.github.com>
2025-05-12 14:26:10 +0900

    [Gold I] Title: 최종 순위, Time: 104 ms, Memory: 13192 KB -BaekjoonHub

--- 백준/Gold/3665. 최종 순위/최종 순위.java
+++ 백준/Gold/3665. 최종 순위/최종 순위.java
@@ -4,79 +4,55 @@
  * 
  * 모든 노드 간에 상대적인 정보가 있으므로 ? 가 출력되는 경우는 없을듯
  * 
+ * 실제 위상 정렬 수행 안하고 degree 만으로 판단 가능
  */
 
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.HashSet;
 
 public class Main {
-    static Node[] tree;
 
     public static void main(String[] args) throws IOException {
         int T = readInt();
         for (int t = 0; t < T; t++) {
             StringBuilder sb = new StringBuilder();
             int n = readInt();
-            tree = new Node[n + 1];
-            for (int i = 1; i <= n; i++)
-                tree[i] = new Node();
 
-            int[] arr = new int[n], inDegree = new int[n + 1];
-            for (int i = 0; i < n; i++) {
-                arr[i] = readInt();
-                for (int j = 0; j < i; j++) {
-                    tree[arr[j]].link.add(arr[i]);
-                    inDegree[arr[i]]++;
-                }
-            }
+            int[] rank = new int[n + 1];
+            for (int i = 1; i <= n; i++)
+                rank[readInt()] = i;
+
+            int[] inDegree = new int[n + 1];
+            for (int i = 1; i <= n; i++)
+                inDegree[i] = rank[i];
 
             int m = readInt();
             for (int i = 0; i < m; i++) {
                 int a = readInt(), b = readInt();
-                if (tree[a].link.contains(b)) {
-                    tree[a].link.remove(b);
-                    inDegree[b]--;
-                    tree[b].link.add(a);
+                if (rank[a] < rank[b]) {
                     inDegree[a]++;
+                    inDegree[b]--;
                 } else {
-                    tree[b].link.remove(a);
                     inDegree[a]--;
-                    tree[a].link.add(b);
                     inDegree[b]++;
                 }
             }
 
-            ArrayDeque<Integer> q = new ArrayDeque<>();
-            for (int i = 1; i <= n; i++)
-                if (inDegree[i] == 0) {
-                    q.offer(i);
-                    sb.append(i).append(' ');
-                }
-
             boolean[] visit = new boolean[n + 1];
-            while (q.size() == 1) {
-                int cur = q.poll();
-                visit[cur] = true;
-                for (int next : tree[cur].link) {
-                    if (--inDegree[next] == 0) {
-                        q.offer(next);
-                        sb.append(next).append(' ');
-                    }
-                }
+            for (int i = 1; i <= n; i++) {
+                if (inDegree[i] <= 0 || inDegree[i] > n)
+                    continue;
+                visit[inDegree[i]] = true;
+                rank[inDegree[i]] = i;
             }
 
             boolean flag = false;
             for (int i = 1; i <= n; i++) {
+                sb.append(rank[i]).append(' ');
                 if (!visit[i])
                     flag = true;
             }
             System.out.println(flag ? "IMPOSSIBLE" : sb);
         }
-    }
-
-    static class Node {
-        HashSet<Integer> link = new HashSet<>();
     }
 
     static int pos, len;
@@ -91,9 +67,7 @@ public class Main {
     }
 
     static int readInt() throws IOException {
-        int c;
-        while ((c = read()) <= 32)
-            ;
+        int c = read();
         int n = c & 15;
         while ((c = read()) > 47)
             n = (n << 3) + (n << 1) + (c & 15);
