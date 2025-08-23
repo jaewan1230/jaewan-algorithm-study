4dcf6c2244e3b3a8f0459a220671bdae443ea83f
박재완 <58254234+jaewan1230@users.noreply.github.com>
2025-08-23 17:33:24 +0900

    [Gold III] Title: 좀비 바이러스, Time: 356 ms, Memory: 52172 KB -BaekjoonHub

--- 백준/Gold/24513. 좀비 바이러스/좀비 바이러스.java
+++ 백준/Gold/24513. 좀비 바이러스/좀비 바이러스.java
@@ -1,91 +1,76 @@
-// 12720 KB, 84 ms
+//문제: 24513번 좀비 바이러스
+//메모리: 51996 KB
+//시간: 328 ms
+
 /*
- * BFS, 1번 2번이 같은 시간에 도착하면 3번 바이러스.
+ * 두 바이러스의 시작점은 겹치지 않으니 처음부터 바이러스3으로 변이되는 일은 없다.
+ * 즉, 바이러스3은 시작시점에서 최소 1시간 이후여야 발생한다.
+ * 또한 두 바이러스가 바이러스3으로 변이되려면 같은 시간에 같은 마을에 전파(완전히 감염)되어야한다.(완전히 감염시키는데 1시간만 걸리기 때문)
+ * 이를 판단하기 위해 마을이 완전히 감염되는 시간을 저장하는 timestamp라는 배열을 사용한다.
+ * map에서 0이 아닌 부분은 어짜피 이미 바이러스가 전파되었거나 백신이 있는 마을이라는 뜻이므로 visited 배열처럼 사용가능하다.
+ * 상술한 내용을 이용하여 BFS를 사용하면 정답
  */
-import java.io.IOException;
+
 import java.util.ArrayDeque;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        int N = readInt(), M = readInt();
-        int[] res = new int[4];
-        int[][] map = new int[N][M], time = new int[N][M];
-
-        ArrayDeque<Data> queue = new ArrayDeque<>();
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < M; j++) {
-                map[i][j] = readInt();
-                if (map[i][j] == 1 || map[i][j] == 2) {
-                    // 종류, 시간, y, x
-                    queue.offer(new Data(map[i][j], 1, i, j));
-                }
-            }
-
-        int[] dx = { -1, 1, 0, 0 }, dy = { 0, 0, -1, 1 };
-        for (int t = 1;; t++) {
-            int cnt = queue.size();
-            if (cnt == 0)
-                break;
-            while (cnt-- > 0) {
-                Data cur = queue.poll();
-                if (map[cur.y][cur.x] == 3)
-                    continue;
-                for (int i = 0; i < 4; i++) {
-                    int ny = cur.y + dy[i];
-                    int nx = cur.x + dx[i];
-                    if (ny < 0 || nx < 0 || ny >= N || nx >= M || map[ny][nx] == -1 || map[ny][nx] == cur.type)
-                        continue;
-                    if (map[ny][nx] == 0) {
-                        map[ny][nx] = cur.type;
-                        time[ny][nx] = cur.time + 1;
-                        queue.offer(new Data(cur.type, time[ny][nx], ny, nx));
-                    } else if (map[ny][nx] != 3 && time[ny][nx] == cur.time + 1) {
-                        map[ny][nx] = 3;
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < M; j++)
-                if (map[i][j] > 0)
-                    res[map[i][j]]++;
-        System.out.printf("%d %d %d", res[1], res[2], res[3]);
-    }
-
-    static class Data {
-        int type, time, y, x;
-
-        public Data(int type, int time, int y, int x) {
+    static class Virus {
+        int x, y, type, time;
+        public Virus(int x, int y, int type, int time){
+            this.x = x;
+            this.y = y;
             this.type = type;
             this.time = time;
-            this.y = y;
-            this.x = x;
+        }
+    }
+    static int[] dx = {1,0,-1,0};
+    static int[] dy = {0,1,0,-1};
+    public static void main(String[] args) throws Exception{
+        int n = nextInt(), m = nextInt();
+        int[][] map = new int[n][m];
+        int[][] timestamp = new int[n][m];
+        ArrayDeque<Virus> queue = new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((map[i][j]=nextInt())<1) continue;
+                if (map[i][j]==1) queue.add(new Virus(i,j,1,0));
+                else queue.add(new Virus(i,j,2,0));
+            }
         }
 
-    }
-
-    static int pos, len;
-    static byte[] buf = new byte[8192];
-
-    static byte read() throws IOException {
-        if (pos == len) {
-            len = System.in.read(buf);
-            pos = 0;
+        while(!queue.isEmpty()){
+            Virus now = queue.poll();
+            int x = now.x, y=now.y;
+            if(map[x][y]==3) continue;
+            for(int dir=0;dir<4;dir++){
+                int nx = x+dx[dir], ny = y+dy[dir];
+                if(nx<0||nx>=n||ny<0||ny>=m) continue;
+                if(map[nx][ny]==0){
+                    queue.add(new Virus(nx,ny,now.type,now.time+1));
+                    map[nx][ny]=now.type;
+                    timestamp[nx][ny]=now.time+1;
+                }else if(timestamp[nx][ny]==now.time+1 && now.type != map[nx][ny]){
+                    map[nx][ny]=3;
+                }
+            }
         }
-        return buf[pos++];
+        int[] count = new int[4];
+        int temp;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if((temp = map[i][j])>0)
+                    count[temp]++;
+        StringBuilder sb = new StringBuilder().append(count[1]).append(' ').append(count[2]).append(' ').append(count[3]);
+        System.out.println(sb);
     }
-
-    static int readInt() throws IOException {
-        int c;
-        while ((c = read()) <= 32)
-            ;
-        boolean negative = c == 45;
-        if (negative)
-            c = read();
-        int n = c & 15;
-        while ((c = read()) > 47)
-            n = (n << 3) + (n << 1) + (c & 15);
-        return negative ? -n : n;
+    static int nextInt() throws Exception{
+        int n,c;
+        boolean flag=false;
+        while((c=System.in.read())<48)
+            flag = c==45;
+        n = c&15;
+        while((c=System.in.read())>47)
+            n = (n<<3) + (n<<1) + (c&15);
+        return flag?-n:n;
     }
 }
