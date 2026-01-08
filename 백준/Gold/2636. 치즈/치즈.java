9c65e77679429fe002370413e7eb36fa9165b332
박재완 <58254234+jaewan1230@users.noreply.github.com>
2026-01-08 14:37:37 +0900

    [Gold IV] Title: 치즈, Time: 64 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,83 +1,100 @@
-
 /*
- * 공기 중에치즈가 모두 녹아 없어지는데 걸리는 시간
- * 모두 녹기 한 시간 전에 남아있는 치즈조각이 놓여 있는 칸의 개수
- * 1, 1부터 치즈 놓고, 0,0 부터 BFS로 바깥 부분만 녹이기.
+치즈
+
+한시간 지나면 접촉한 곳 녹음
+
+모두 녹아 없어지는데 걸리는 시간, 모두 녹기 한 시간 전 남아있는 치즈 조각 개수 구하기
+
+---
+
+BFS로 시뮬레이션 돌려서 외곽부터 녹여나가기
+
+각 시간별 개수 저장해서 모두 녹기 한 시간 전 개수 출력
+
  */
+
 import java.io.IOException;
 import java.util.ArrayDeque;
 
 public class Main {
     static int N, M;
-    static int[][] map;
-    static boolean[][] visit;
-    static ArrayDeque<int[]> q, q2 = new ArrayDeque<>();
+    static int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
+    static boolean[][] cheese;
 
     public static void main(String[] args) throws IOException {
         N = readInt();
         M = readInt();
-        map = new int[N + 2][M + 2];
-        for (int i = 1; i <= N; i++)
-            for (int j = 1; j <= M; j++)
-                map[i][j] = readInt();
-        int prev = 0;
-        for (int time = 0;; time++) {
-            meltingCheese(time);
-            if (q2.size() == 0) {
-                System.out.printf("%d\n%d", time, prev);
-                break;
+        cheese = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cheese[i][j] = readInt() == 1;
             }
-            prev = q2.size();
         }
+        BFS();
     }
 
-    // 0, 0 에서 출발해 공기와 맞닿아 있는 치즈를 녹인다.
-    static int meltingCheese(int time) {
-        int cnt = 0;
-        int[] dy = { 0, 0, -1, 1 }, dx = { -1, 1, 0, 0 };
+    public static void BFS() {
         ArrayDeque<int[]> q = new ArrayDeque<>();
-        if (time == 0) {
-            visit = new boolean[N + 2][M + 2];
-            q.offer(new int[] { 0, 0 });
-            visit[0][0] = true;
-        } else
-            while (!q2.isEmpty())
-                q.offer(q2.poll());
+        boolean[][] visit = new boolean[N][M];
+        int time = 1, cheeseCnt = 0, prevCheeseCnt = 0;
+        // time 에 녹음
+        q.add(new int[] { 0, 0, time });
+        visit[0][0] = true;
+
         while (!q.isEmpty()) {
             int[] cur = q.poll();
+            if(cur[2] > time) {
+                // 시간 변경
+                prevCheeseCnt = cheeseCnt;
+                cheeseCnt = 0;
+                time = cur[2];
+            }
+
+            int y = cur[0], x = cur[1];
+
             for (int dir = 0; dir < 4; dir++) {
-                int ny = cur[0] + dy[dir];
-                int nx = cur[1] + dx[dir];
-                if (ny < 0 || nx < 0 || ny > N + 1 || nx > M + 1)
+                int ny = y + dy[dir], nx = x + dx[dir];
+
+                if (nx < 0 || ny < 0 || ny >= N || nx >= M || visit[ny][nx])
                     continue;
-                if (visit[ny][nx])
-                    continue;
+
                 visit[ny][nx] = true;
-                switch (map[ny][nx]) {
-                    case 0:
-                        q.offer(new int[] { ny, nx });
-                        break;
-                    case 1:
-                        q2.offer(new int[] { ny, nx });
-                        map[ny][nx] = 0;
+
+                if (cheese[ny][nx]) {
+                    cheese[ny][nx] = false; // 녹음
+                    cheeseCnt++;
+                    q.addLast(new int[] { ny, nx, time + 1 });
+                } else {
+                    q.addFirst(new int[] { ny, nx, time });
                 }
             }
         }
-        return cnt;
+
+        System.out.println(time - 1);
+        System.out.println(prevCheeseCnt);
     }
 
-    static int readInt() throws IOException {
-        int c;
-        do {
-            c = System.in.read();
-        } while (c <= 32);
-        int n = c & 15;
+    public static int pos, len;
+    public static byte[] buf = new byte[8192];
 
-        c = System.in.read();
-        while (c > 47) {
-            n = (n << 3) + (n << 1) + (c & 15);
-            c = System.in.read();
+    public static byte read() throws IOException {
+        if (pos == len) {
+            len = System.in.read(buf);
+            pos = 0;
         }
-        return n;
+        return buf[pos++];
+    }
+
+    public static int readInt() throws IOException {
+        int c;
+        while ((c = read()) <= 32)
+            ;
+        boolean negative = c == 45;
+        if (negative)
+            c = read();
+        int n = c & 15;
+        while ((c = read()) > 47)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return negative ? -n : n;
     }
 }
